fff311fab0a21525f4b233f2277b3fd7947fe718
Joao Barreto <joao.barreto@tecnico.pt>
2024-02-28 10:33:04 +0000

    Testes para a etapa 1.1.

---
+++ initial-code/tests/run_tests.sh
@@ -0,0 +1,31 @@
+################################################
+### PATHS (feel free to tweak paths accordingly)
+CLI_PATH=${PWD}/../Client
+TESTS_FOLDER=${PWD}
+TESTS_OUT_EXPECTED=${TESTS_FOLDER}/expected
+TESTS_OUTPUT=${TESTS_FOLDER}/test-outputs
+################################################
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+################################################
+
+rm -rf $TESTS_OUTPUT
+mkdir -p $TESTS_OUTPUT
+
+cd $CLI_PATH
+for i in {1..5}
+do
+    TEST=$(printf "%02d" $i)
+    mvn compile exec:java < ${TESTS_FOLDER}/input$TEST.txt > ${TESTS_OUTPUT}/out$TEST.txt
+
+    DIFF=$(diff ${TESTS_OUTPUT}/out$TEST.txt ${TESTS_OUT_EXPECTED}/out$TEST.txt) 
+    if [ "$DIFF" != "" ] 
+    then
+        echo "${RED}[$TEST] TEST FAILED${NC}"
+    else
+        echo "${GREEN}[$TEST] TEST PASSED${NC}"
+    fi
+done
+
+echo "Check the outputs of each test in ${TESTS_OUTPUT}."
