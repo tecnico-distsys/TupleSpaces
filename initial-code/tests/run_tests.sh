2ee7706199ee7626ecf06b42aacefa7af980389d
Joao Barreto <joao.barreto@tecnico.pt>
2024-03-13 15:30:50 +0000

    Removed input06.txt, which had issues

--- initial-code/tests/run_tests.sh
+++ initial-code/tests/run_tests.sh
@@ -14,7 +14,7 @@ rm -rf $TESTS_OUTPUT
 mkdir -p $TESTS_OUTPUT
 
 cd $CLI_PATH
-for i in {1..6}
+for i in {1..5}
 do
     TEST=$(printf "%02d" $i)
     time mvn --quiet compile exec:java < ${TESTS_FOLDER}/input$TEST.txt > ${TESTS_OUTPUT}/out$TEST.txt
